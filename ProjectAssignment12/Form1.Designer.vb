4800fa44684312ad62ded511cb11b6e213067bfc
Vahn19 <33344189+Vahn19@users.noreply.github.com>
2020-08-29 17:42:48 +0700

    Add files via upload

---
+++ ProjectAssignment12/Form1.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.picturebox_main = New System.Windows.Forms.PictureBox()
+        Me.btn_start = New System.Windows.Forms.Button()
+        Me.txtbox_velocity = New System.Windows.Forms.TextBox()
+        Me.lbl_velocity = New System.Windows.Forms.Label()
+        Me.lbl_torque = New System.Windows.Forms.Label()
+        Me.txtbox_torque = New System.Windows.Forms.TextBox()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.lbl_coordinate = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.timer_anim = New System.Windows.Forms.Timer(Me.components)
+        Me.btn_reset = New System.Windows.Forms.Button()
+        Me.btn_placecar = New System.Windows.Forms.Button()
+        Me.btn_placewaypoint = New System.Windows.Forms.Button()
+        Me.btn_stop = New System.Windows.Forms.Button()
+        CType(Me.picturebox_main, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'picturebox_main
+        '
+        Me.picturebox_main.BackColor = System.Drawing.Color.LightGray
+        Me.picturebox_main.Location = New System.Drawing.Point(12, 27)
+        Me.picturebox_main.Name = "picturebox_main"
+        Me.picturebox_main.Size = New System.Drawing.Size(1061, 579)
+        Me.picturebox_main.TabIndex = 0
+        Me.picturebox_main.TabStop = False
+        '
+        'btn_start
+        '
+        Me.btn_start.BackColor = System.Drawing.Color.Lime
+        Me.btn_start.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_start.Location = New System.Drawing.Point(1092, 226)
+        Me.btn_start.Name = "btn_start"
+        Me.btn_start.Size = New System.Drawing.Size(101, 36)
+        Me.btn_start.TabIndex = 1
+        Me.btn_start.Text = "Start"
+        Me.btn_start.UseVisualStyleBackColor = False
+        '
+        'txtbox_velocity
+        '
+        Me.txtbox_velocity.Location = New System.Drawing.Point(1092, 57)
+        Me.txtbox_velocity.Name = "txtbox_velocity"
+        Me.txtbox_velocity.Size = New System.Drawing.Size(123, 22)
+        Me.txtbox_velocity.TabIndex = 2
+        Me.txtbox_velocity.Text = "0"
+        '
+        'lbl_velocity
+        '
+        Me.lbl_velocity.AutoSize = True
+        Me.lbl_velocity.Location = New System.Drawing.Point(1092, 29)
+        Me.lbl_velocity.Name = "lbl_velocity"
+        Me.lbl_velocity.Size = New System.Drawing.Size(57, 17)
+        Me.lbl_velocity.TabIndex = 3
+        Me.lbl_velocity.Text = "Velocity"
+        '
+        'lbl_torque
+        '
+        Me.lbl_torque.AutoSize = True
+        Me.lbl_torque.Location = New System.Drawing.Point(1092, 94)
+        Me.lbl_torque.Name = "lbl_torque"
+        Me.lbl_torque.Size = New System.Drawing.Size(54, 17)
+        Me.lbl_torque.TabIndex = 4
+        Me.lbl_torque.Text = "Torque"
+        '
+        'txtbox_torque
+        '
+        Me.txtbox_torque.Location = New System.Drawing.Point(1092, 128)
+        Me.txtbox_torque.Name = "txtbox_torque"
+        Me.txtbox_torque.Size = New System.Drawing.Size(123, 22)
+        Me.txtbox_torque.TabIndex = 5
+        Me.txtbox_torque.Text = "0"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.lbl_coordinate})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 619)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(1255, 25)
+        Me.StatusStrip1.TabIndex = 6
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'lbl_coordinate
+        '
+        Me.lbl_coordinate.Name = "lbl_coordinate"
+        Me.lbl_coordinate.Size = New System.Drawing.Size(47, 20)
+        Me.lbl_coordinate.Text = "( X, Y)"
+        '
+        'timer_anim
+        '
+        '
+        'btn_reset
+        '
+        Me.btn_reset.BackColor = System.Drawing.Color.Cyan
+        Me.btn_reset.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_reset.Location = New System.Drawing.Point(1092, 277)
+        Me.btn_reset.Name = "btn_reset"
+        Me.btn_reset.Size = New System.Drawing.Size(101, 36)
+        Me.btn_reset.TabIndex = 7
+        Me.btn_reset.Text = "Reset"
+        Me.btn_reset.UseVisualStyleBackColor = False
+        '
+        'btn_placecar
+        '
+        Me.btn_placecar.BackColor = System.Drawing.Color.Black
+        Me.btn_placecar.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_placecar.ForeColor = System.Drawing.Color.White
+        Me.btn_placecar.Location = New System.Drawing.Point(1092, 506)
+        Me.btn_placecar.Name = "btn_placecar"
+        Me.btn_placecar.Size = New System.Drawing.Size(101, 36)
+        Me.btn_placecar.TabIndex = 8
+        Me.btn_placecar.Text = "Place Car"
+        Me.btn_placecar.UseVisualStyleBackColor = False
+        '
+        'btn_placewaypoint
+        '
+        Me.btn_placewaypoint.BackColor = System.Drawing.Color.Red
+        Me.btn_placewaypoint.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_placewaypoint.Location = New System.Drawing.Point(1092, 419)
+        Me.btn_placewaypoint.Name = "btn_placewaypoint"
+        Me.btn_placewaypoint.Size = New System.Drawing.Size(101, 70)
+        Me.btn_placewaypoint.TabIndex = 9
+        Me.btn_placewaypoint.Text = "Place Waypoints"
+        Me.btn_placewaypoint.UseVisualStyleBackColor = False
+        '
+        'btn_stop
+        '
+        Me.btn_stop.BackColor = System.Drawing.Color.Red
+        Me.btn_stop.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_stop.ForeColor = System.Drawing.Color.Black
+        Me.btn_stop.Location = New System.Drawing.Point(1092, 328)
+        Me.btn_stop.Name = "btn_stop"
+        Me.btn_stop.Size = New System.Drawing.Size(101, 36)
+        Me.btn_stop.TabIndex = 10
+        Me.btn_stop.Text = "Stop"
+        Me.btn_stop.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1255, 644)
+        Me.Controls.Add(Me.btn_stop)
+        Me.Controls.Add(Me.btn_placewaypoint)
+        Me.Controls.Add(Me.btn_placecar)
+        Me.Controls.Add(Me.btn_reset)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.txtbox_torque)
+        Me.Controls.Add(Me.lbl_torque)
+        Me.Controls.Add(Me.lbl_velocity)
+        Me.Controls.Add(Me.txtbox_velocity)
+        Me.Controls.Add(Me.btn_start)
+        Me.Controls.Add(Me.picturebox_main)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.picturebox_main, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picturebox_main As PictureBox
+    Friend WithEvents btn_start As Button
+    Friend WithEvents txtbox_velocity As TextBox
+    Friend WithEvents lbl_velocity As Label
+    Friend WithEvents lbl_torque As Label
+    Friend WithEvents txtbox_torque As TextBox
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents lbl_coordinate As ToolStripStatusLabel
+    Friend WithEvents timer_anim As Timer
+    Friend WithEvents btn_placecar As Button
+    Friend WithEvents btn_placewaypoint As Button
+    Friend WithEvents btn_stop As Button
+    Friend WithEvents btn_reset As Button
+End Class
---
+++ ProjectAssignment12/Form1.vb
@@ -0,0 +1,148 @@
+﻿Public Class Form1
+
+    Dim x, y, xc, yc, x_c, y_c As Double
+    Dim cnt As Integer = 0
+    Dim max As Integer
+    Dim screen As Graphics
+    Dim waypoint = New List(Of Rectangle)
+    Dim placement As Boolean = False
+    Dim first_placement As Boolean = False
+    Dim go As Boolean = True
+    Dim v, vx, vy, dx, dy, pos As Double
+    Dim a As Double
+
+    Private Sub btn_stop_Click(sender As Object, e As EventArgs) Handles btn_stop.Click
+        go = False
+        a = 1
+    End Sub
+
+    Private Sub timer_anim_Tick(sender As Object, e As EventArgs) Handles timer_anim.Tick
+        If cnt < waypoint.count Then
+            dx = waypoint(cnt).x - xc
+            dy = waypoint(cnt).y - yc
+
+            Dim d = Math.Sqrt(dx * dx + dy * dy)
+            Dim torq = (vx * dy - vy * dx)
+            If torq < 0 Then
+                pos -= txtbox_torque.Text
+            ElseIf torq > 0 Then
+                pos += txtbox_torque.Text
+            End If
+
+            If v + 90 > d AndAlso cnt = waypoint.count - 1 Then
+                a = 0.8
+                go = False
+            End If
+
+            start(go)
+
+                vx = v * Math.Cos(pos * (Math.PI / 180))
+                vy = v * Math.Sin(pos * (Math.PI / 180))
+
+                screen.DrawEllipse(Pens.LightGray, CInt(xc), CInt(yc), 20, 20)
+                screen.FillEllipse(Brushes.LightGray, CInt(xc), CInt(yc), 20, 20)
+
+                xc += vx
+                yc += vy
+
+                screen.DrawEllipse(Pens.Black, CInt(xc), CInt(yc), 20, 20)
+                screen.FillEllipse(Brushes.Black, CInt(xc), CInt(yc), 20, 20)
+
+                If v > d Then
+                    screen.DrawEllipse(Pens.LightGray, waypoint(cnt).x, waypoint(cnt).y, 15, 15)
+                    screen.FillEllipse(Brushes.LightGray, waypoint(cnt).x, waypoint(cnt).y, 15, 15)
+                    cnt += 1
+                End If
+            Else
+                timer_anim.Enabled = False
+        End If
+    End Sub
+
+    Function start(stats As Boolean)
+        If stats Then
+            If v < max Then
+                v += a
+            End If
+        Else
+            v -= a
+            If v < 0 Then
+                v = 0
+                timer_anim.Enabled = False
+            End If
+        End If
+    End Function
+
+    Private Sub btn_reset_Click(sender As Object, e As EventArgs) Handles btn_reset.Click
+        timer_anim.Enabled = False
+        cnt = 0
+        pos = 0
+        xc = 0
+        yc = 0
+
+        go = False
+        waypoint.clear()
+        picturebox_main.Refresh()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        screen = picturebox_main.CreateGraphics()
+        screen.DrawLine(Pens.Red, 50, 50, 400, 400)
+    End Sub
+
+    Private Sub btn_placewaypoint_Click(sender As Object, e As EventArgs) Handles btn_placewaypoint.Click
+        placement = False
+    End Sub
+
+    Private Sub btn_start_Click(sender As Object, e As EventArgs) Handles btn_start.Click
+        If txtbox_velocity.Text = 0 Or txtbox_velocity.Text < 0 Then
+            MsgBox("Please input velocity more than 0")
+        ElseIf txtbox_torque.Text = 0 Or txtbox_torque.Text < 0 Then
+            MsgBox("Please input torque more than 0")
+        ElseIf Not first_placement Then
+            MsgBox("Please place the car")
+        ElseIf waypoint.count = 0 Then
+            MsgBox("Please place add waypoints")
+        Else
+            v = 0
+            max = txtbox_velocity.Text
+            a = 0.5
+            pos = 0
+
+            go = True
+            timer_anim.Enabled = True
+        End If
+    End Sub
+
+    Private Sub picturebox_main_MouseMove(sender As Object, e As MouseEventArgs) Handles picturebox_main.MouseMove
+        x = e.X
+        y = e.Y
+        lbl_coordinate.Text = (x & ", " & y)
+    End Sub
+
+    Private Sub btn_placecar_Click(sender As Object, e As EventArgs) Handles btn_placecar.Click
+        placement = True
+    End Sub
+
+    Private Sub picturebox_main_Click(sender As Object, e As EventArgs) Handles picturebox_main.Click
+        If placement = False Then
+            Dim set_waypoint = New Rectangle(x, y, 15, 15)
+            screen.DrawEllipse(Pens.Red, CInt(x), CInt(y), 15, 15)
+            screen.FillEllipse(Brushes.Red, CInt(x), CInt(y), 15, 15)
+            waypoint.Add(set_waypoint)
+        Else
+            If Not first_placement Then
+                first_placement = True
+            Else
+                screen.DrawEllipse(Pens.LightGray, CInt(xc), CInt(yc), 20, 20)
+                screen.FillEllipse(Brushes.LightGray, CInt(xc), CInt(yc), 20, 20)
+            End If
+            x_c = x
+            y_c = y
+            screen.DrawEllipse(Pens.Black, CInt(x_c), CInt(y_c), 20, 20)
+            screen.FillEllipse(Brushes.Black, CInt(x_c), CInt(y_c), 20, 20)
+
+            xc = x
+            yc = y
+        End If
+    End Sub
+End Class
